f8c97f73069e3b68e770b3d23f7041116194ba2a
hiba <hibajira123@gmaim.com>
2025-06-15 17:35:02 +0100

    update

--- alx-project-0x05/context/CountContext.tsx
+++ alx-project-0x05/context/CountContext.tsx
@@ -33,18 +33,3 @@ export const useCount = () => {
 
   return context
 }
-Modify your App Document (_app.tsx) to look like this
-import Layout from "@/components/layouts/Layout";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { CountProvider } from "@/context/CountContext";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <CountProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </CountProvider>
-  )
-}
